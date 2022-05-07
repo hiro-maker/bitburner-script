8698aa19c53d9ba74c2e861f7d9c884b0b238aa4
hiro <gensousuikoden50@gmail.com>
2022-05-07 19:58:32 +0900

    wip

--- my_src/auto_nuke.js
+++ my_src/auto_nuke.js
@@ -1,18 +1,24 @@
-// TODO  Did you forget a ns.sleep(x)?
-// Where infinite loop?
+const errorType = {
+	FIND_TARGET: 0,
+	HAS_ROOT: 1,
+	LOW_HACKING_LEVEL: 2,
+	LOW_HACKING_LEVEL_OVER: 3,
+	LOW_PORT_TOOL: 4,
+	UNKNOW: 999,
+}
+
 /** @param {NS} ns */
 export async function main(ns) {
+	var target = [errorType.UNKNOW, ns.getHostname()]
 	while (true) {
-		var hackinglevel = ns.getHackingLevel()
-		var target = await getTargetServer(ns.getHostname(), hackinglevel)
-		if (target != null) {
-			ns.print(target)
-			startNuke(target)
-			await runScript(target)
+		target = await getTargetServer(target[1], ns.getHackingLevel())
+		if (target[0] == errorType.FIND_TARGET) {
+			startNuke(target[1])
+			await runScript(target[1])
 		}
 		await ns.sleep(60 * 1000)
 	}
-	
+
 	async function getTargetServer(host, hackinglevel) {
 		var servers = ns.scan(host)
 		var result
@@ -20,33 +26,38 @@ export async function main(ns) {
 			if (index == 0) {
 				continue
 			}
-			ns.print("--start--")
-			var target = servers[index]
-			var targetRequiredLevel = ns.getServerRequiredHackingLevel(target)
-			ns.print(target)
-			if (ns.hasRootAccess(target)) {
-				ns.print("hasRoot")
-			} else if (targetRequiredLevel > hackinglevel) {
-				ns.print("low hackinglevel")
-				if (targetRequiredLevel - hackinglevel > 100) {
-					ns.print("low hackinglevel over")
-					continue
-				}
-			} else if (ns.getServerNumPortsRequired(target) > countPortTools()) {
-				ns.print("low port tools")
-			} else {
-				ns.print("hit target !!!")
-				result = target
+			result = servers[index]
+			let type = isTargetServer(result, hackinglevel)
+			if (type == errorType.LOW_HACKING_LEVEL_OVER) {
+				continue
 			}
-			if (result == null) {
-				await ns.sleep(1 * 1000)
-				result = await getTargetServer(target, hackinglevel)
-			}
-			ns.print("--end--")
 		}
+		// TODO required last server flg.
+		return [type, result]
+	}
+
+	function isTargetServer(target, hackinglevel) {
+		var result = errorType.UNKNOW
+		ns.print("--start--")
+		var targetRequiredLevel = ns.getServerRequiredHackingLevel(target)
+		if (ns.hasRootAccess(target)) {
+			result = errorType.HAS_ROOT
+		} else if (targetRequiredLevel > hackinglevel) {
+			result = errorType.LOW_HACKING_LEVEL
+			if (targetRequiredLevel - hackinglevel > 100) {
+				result = errorType.LOW_HACKING_LEVEL_OVER
+			}
+		} else if (ns.getServerNumPortsRequired(target) > countPortTools()) {
+			result = errorType.LOW_PORT_TOOL
+		} else {
+			result = errorType.FIND_TARGET
+		}
+		ns.print(target + " is " + result.toString())
+		ns.print("--end--")
 		return result
 	}
-	
+
+
 	function countPortTools() {
 		var result = 0
 		if (ns.fileExists("BruteSSH.exe", "home")) {
@@ -66,7 +77,7 @@ export async function main(ns) {
 		}
 		return result
 	}
-	
+
 	function startNuke(target) {
 		// ready nuke.
 		if (ns.fileExists("BruteSSH.exe", "home")) {
@@ -87,21 +98,21 @@ export async function main(ns) {
 		ns.nuke(target);
 		//ns.installBackdoor(target)
 	}
-	
+
 	async function runScript(target) {
 		var script1 = "early-hack-template.js"
 		var script2 = "hackloop.js"
-	
+
 		// scp script
 		await ns.scp(script1, target)
 		await ns.scp(script2, target)
-	
+
 		// run script
 		ns.exec(script2, target)
-	
+
 		var maxRam = ns.getServerMaxRam(target)
 		var script1Ram = ns.getScriptRam(script1)
-	
+
 		var id = 0;
 		while (maxRam - ns.getServerUsedRam(target) >= script1Ram) {
 			ns.exec(script1, target, 1, id)
