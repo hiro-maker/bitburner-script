c6c4918c5addf3b869e33c67a907df5329eba5eb
hiro <gensousuikoden50@gmail.com>
2022-05-09 15:23:21 +0900

    wip

--- contract/totalWaysToSumII.js
+++ contract/totalWaysToSumII.js
@@ -3,7 +3,14 @@ export async function main(ns) {
     const host = ((args0) => args0 !== undefined ? args0 : "")(ns.args[0])
     const filename = ((args1) => args1 !== undefined ? args1 : "")(ns.args[1])
     const data = ns.codingcontract.getData(filename, host);
-    ns.tprint(_getCombinationSum(data[0], data[1]))
+
+    const isAutoSolve = ((args2) => args2 !== undefined ? args2 : false)(ns.args[2])
+    const answer = _getCombinationSum(data[0], data[1])
+    if (isAutoSolve) {
+        ns.tprint(ns.codingcontract.attempt(answer, filename, host, { returnReward: true }))
+    } else {
+        ns.tprint(answer)
+    }
 }
 
 function _getCombinationSum(target, candidates) {
