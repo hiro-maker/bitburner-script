98f1307c6c18b01a7f70843733cd4f05bb4699df
hiro <gensousuikoden50@gmail.com>
2022-05-19 14:25:34 +0900

    init

---
+++ corporation/create.js
@@ -0,0 +1,27 @@
+/** @param {NS} ns */
+export async function main(ns) {
+    /**
+     * acceptInvestmentOffer()	Accept investment based on you companies current valuation
+bribe(factionName, amountCash, amountShares)	Bribe a faction
+buyBackShares(amount)	Buyback Shares
+createCorporation(corporationName, selfFund)	Create a Corporation
+expandCity(divisionName, cityName)	Expand to a new city
+expandIndustry(industryType, divisionName)	Expand to a new industry
+getBonusTime()	Get bonus time.“Bonus time” is accumulated when the game is offline or if the game is inactive in the browser.“Bonus time” makes the game progress faster.
+getCorporation()	Get corporation data
+getDivision(divisionName)	Get division data
+getExpandCityCost()	Gets the cost to expand into a new city
+getExpandIndustryCost(industryName)	Gets the cost to expand into a new industry
+getInvestmentOffer()	Get an offer for investment based on you companies current valuation
+getUnlockUpgradeCost(upgradeName)	Gets the cost to unlock a one time unlockable upgrade
+getUpgradeLevel(upgradeName)	Get the level of a levelable upgrade
+getUpgradeLevelCost(upgradeName)	Gets the cost to unlock the next level of a levelable upgrade
+goPublic(numShares)	Go public
+hasUnlockUpgrade(upgradeName)	Check if you have a one time unlockable upgrade
+issueDividends(percent)	Issue dividends
+levelUpgrade(upgradeName)	Level an upgrade.
+sellShares(amount)	Sell Shares
+unlockUpgrade(upgradeName)	Unlock an upgrade
+https://github.com/danielyxie/bitburner/blob/dev/markdown/bitburner.corporation.md
+     */
+}
